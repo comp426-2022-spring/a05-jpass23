3141acf61cf914ca9157028b1fa9041b3abed02e
jpass23 <jpass23@live.unc.edu>
2022-04-28 15:33:18 -0400

    made a change

--- public/main.js
+++ public/main.js
@@ -7,7 +7,24 @@ function btnSelect(selector){
     document.getElementById(selector).setAttribute("class", "active")
 }
 // Flip one coin and show coin image to match result when button clicked
-
+const coin = document.getElementById("coin")
+// Add event listener for coin button
+			coin.addEventListener("click", flipCoin)
+			function flipCoin() {
+                fetch('http://localhost:5000/app/flip/', {mode: 'cors'})
+  				.then(function(response) {
+    			  return response.json();
+  				})
+				.then(function(result) {
+					console.log(result);
+					document.getElementById("result").innerHTML = result.flip;
+					document.getElementById("quarter").setAttribute("src", result.flip+".jpg");
+					coin.disabled = true
+				})
+//				let flip = "FLIPPED"
+//				document.getElementById("coin").innerHTML = flip;
+//				console.log("Coin has been flipped. Result: "+ flip)
+			}
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
 
